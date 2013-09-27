20d6b070bdfba5a8491f68822fe2443265bec14b
Jon Packer <contact@jonpacker.com>
2013-09-27 11:25:49 +0200

    Use pathname normalizer module

--- ppp.js
+++ ppp.js
@@ -1,7 +1,7 @@
-var normalize = require('path').normalize;
+var normalize = require('normalize-pathname');
 module.exports = function pathPrefixProxy(prefix) {
   // normalize to always having no leading slash and 1 trailing slash
-  prefix = normalize('/' + prefix).replace(/\/$/, "");
+  prefix = normalize(prefix);
   return function(req, res, next) {
     req.path = req.path.replace(prefix, '/');
     req.app.handle(req, res, next);
